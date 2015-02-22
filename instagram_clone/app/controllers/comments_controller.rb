b5982f13cc009c5be586e8a97e467e9c73849e0b
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-22 16:01:02 +0000

    user can delete comments

--- instagram_clone/app/controllers/comments_controller.rb
+++ instagram_clone/app/controllers/comments_controller.rb
@@ -22,6 +22,13 @@ class CommentsController < ApplicationController
     redirect_to '/posts'
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to '/posts'
+  end
+
   def comment_params
     params.require(:comment).permit(:comment)
   end
