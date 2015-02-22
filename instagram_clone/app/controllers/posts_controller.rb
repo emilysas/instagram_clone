448fac3c8a5bd1176ddcc748819911d355dac0cd
Emily Sas <emily.bronwen.sas@gmail.com>
2015-02-22 11:30:28 +0000

    tests failing, but app working - user can add, view and edit photographs

--- instagram_clone/app/controllers/posts_controller.rb
+++ instagram_clone/app/controllers/posts_controller.rb
@@ -12,7 +12,21 @@ class PostsController < ApplicationController
     redirect_to '/posts'
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts'
+  end
+
   def post_params
-    params.require(:post).permit(:image)
+    params.require(:post).permit(:image, :title)
   end
 end
--- instagram_clone/spec/features/post_feature_spec.rb
+++ instagram_clone/spec/features/post_feature_spec.rb
@@ -3,4 +3,34 @@ feature 'Post' do
     visit '/posts'
     expect(page).to have_content 'No posts yet!'
   end
+
+  # scenario 'user can leave a post' do
+  #   visit '/posts'
+  #   click_link 'Add a photograph'
+  #   attach_file 'Image' => (Rails.root + 'spec/fixtures/images/profilepic.png')
+  #   fill_in 'Title', with: 'Me!'
+  #   click_button 'Create Post'
+  #   expect(page.find('img')['src']).to include('profilepic.jpg')
+  #   expect(page).to have_content 'Me!'
+  # end
+  
+  # scenario 'user can view posts' do
+  #   Post.create(image: open(Rails.root+'spec/fixtures/images/profilepic.png'), title: 'Me')
+  #   visit '/posts'
+  #   click_link 'Me'
+  #   expect(current_path).to be('/posts/#{id}')
+  # end
+
+  scenario 'user can edit posts' do
+    Post.create(image: open(Rails.root+'spec/fixtures/images/profilepic.png'), title: 'Me')
+    visit '/posts'
+    click_link 'Me'
+    click_link 'Edit Post'
+    fill_in "Title", with: "vintage hair"
+    click_button 'Create Post'
+    expect(page).to have_content "vintage hair"
+    expect(current_path).to be '/posts'
+  end
 end
+
+# Post.new :image => File.new(Rails.root + 'spec/fixtures/images/profilepic.png')
